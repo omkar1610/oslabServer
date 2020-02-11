1e7dd2f15b17baa8432720c1dcdc11e95ac49143
Omkar Ashrit <omkar.ashrit@gmail.com>
2020-02-12 00:34:21 +0530

    2020-02-12

--- day1/systemInfo.c
+++ day1/systemInfo.c
@@ -20,6 +20,7 @@ void printCPU(){
         if(strstr(line, "cpu MHz")!=NULL)
             printf("%s", line);
     }
+    printf("\n");
 
     fclose(fp);
 }
@@ -52,40 +53,70 @@ int main(int argc, char const *argv[])
             break;
         
         case 2:
-            system("echo Kernel Version \\:  && cat /proc/sys/kernel/osrelease");
+            system("awk '{print \"Kernel Version = \" $0}' /proc/sys/kernel/osrelease");
+            printf("\n");
             break;
 
         case 3:
-            system("cat /proc/uptime");
+            system("echo The time in day:hr:min:sec when the system was last booted = && awk ' {print int($1/86400) \":\"int(($1%86400)/3600) \":\" int((($1%86400)%3600)/60) \":\" int($1%60)}' /proc/uptime");
+            printf("\n");
             break;
 
         case 4:
-            system("echo Average Load on System last 15 mins && awk '{print $3}' /proc/loadavg");
+            system("awk '{print \"Average Load on System last 15 mins = \" $3 }' /proc/loadavg");
+            printf("\n");
             break;
 
         case 5:
-            system("awk '/Mem[TF]/ {print $0}' /proc/meminfo");
+            system("echo Memory Information : && awk '/Mem[TF]/ {print $0}' /proc/meminfo");
+            printf("\n");
             break;
 
         case 6:
+            system("echo Swap Information : && awk '{if($1 ~ /SwapT/) {tot = $2; print \"Swap Total = \" $2, $3;} else if($1 ~ /SwapF/) {print \"Swap Used = \" tot-$2, $3}}' /proc/meminfo");
+            printf("\n");
             break;
 
         case 7:
-            system("cat /proc/swaps");
+            system("echo Swap Partition Information\\(Filename = Size\\): && awk 'NR>1 {print $1 \" = \"$3 ,\"Kb\"}' /proc/swaps");
+            printf("\n");
             break;
 
         case 8:
+            system("awk '{if ($1 ~ /cpu$/) print \"CPU time User and Kernel Mode(in USER_HX) = \" $2, $4 }' /proc/stat");
+            printf("\n");
             break;
 
         case 9:
-            system("awk '/ctxt/ {print \"Total number of context switches = \" $2}' /proc/stat");
+            system("awk '/ctxt/ {print \"Total number of context switches = \" $2 }' /proc/stat");
+            printf("\n");
             break;
 
         case 10:
-            system("awk '/intr/ {print \"Total number of Interrupts = \" $2}' /proc/stat");
+            system("awk '/intr/ {print \"Total number of Interrupts = \" $2 }' /proc/stat");
+            printf("\n");
             break;
 
         case 11:
+            printCPU();
+            system("awk '{print \"Kernel Version = \" $0}' /proc/sys/kernel/osrelease");
+            printf("\n");
+            system("echo The time in day:hr:min:sec when the system was last booted = && awk ' {print int($1/86400) \":\"int(($1%86400)/3600) \":\" int((($1%86400)%3600)/60) \":\" int($1%60)}' /proc/uptime");
+            printf("\n");
+            system("awk '{print \"Average Load on System last 15 mins = \" $3 }' /proc/loadavg");
+            printf("\n");
+            system("echo Memory Information : && awk '/Mem[TF]/ {print $0}' /proc/meminfo");
+            printf("\n");
+            system("echo Swap Information : && awk '{if($1 ~ /SwapT/) {tot = $2; print \"Swap Total = \" $2, $3;} else if($1 ~ /SwapF/) {print \"Swap Used = \" tot-$2, $3}}' /proc/meminfo");
+            printf("\n");
+            system("echo Swap Partition Information\\(Filename = Size\\): && awk 'NR>1 {print $1 \" = \"$3 ,\"Kb\"}' /proc/swaps");
+            printf("\n");
+            system("awk '{if ($1 ~ /cpu$/) print \"CPU time User and Kernel Mode(in USER_HX) = \" $2, $4 }' /proc/stat");
+            printf("\n");
+            system("awk '/ctxt/ {print \"Total number of context switches = \" $2 }' /proc/stat");
+            printf("\n");
+            system("awk '/intr/ {print \"Total number of Interrupts = \" $2 }' /proc/stat");
+            printf("\n");
             break;
 
         case 12:
