96cd75f782b530133f1a740f5025e3f54e035d6e
omkar1610 <omkar.ashrit@gmail.com>
2020-02-13 18:37:42 +0530

    2020-02-13

---
+++ day1/VirtualMemoryProjectTemplate/head_project.h
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+#define TLB_SIZE 16
+#define PAGES 256
+#define PAGE_MASK 255
+
+#define PAGE_SIZE 256
+#define OFFSET_BITS 8
+#define OFFSET_MASK 255
+
+#define MEMORY_SIZE PAGES * PAGE_SIZE
+
+// Max number of characters per line of input file to read.
+#define BUFFER_SIZE 10
+
+struct tlbentry {
+    unsigned char logical;
+    unsigned char physical;
+};
+
+class project
+{
+private:
+
+public:
+	
+};
---
+++ day1/VirtualMemoryProjectTemplate/project.cpp
@@ -0,0 +1,8 @@
+#include "head_project.h"
+
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
